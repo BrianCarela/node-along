7de99cfe3155bf216b5cd9ea804e448f219a2c6e
BrianCarela <briancarela@gmail.com>
2021-02-24 12:13:18 -0500

    sup

--- greet.js
+++ greet.js
@@ -1,1 +1,7 @@
 const getInput = require("./get-input.js");
+
+// 1. get the user's input and save it in a variable
+const str = getInput();
+
+// 2. Print it back out to the console based on user input
+console.log("Sup " + str + "!")
